b75da38dddfc6cc73fa188fbcf4903188d12aee1
Jared Harbison <jared.harbison@gmail.com>
2018-12-15 21:17:02 +0000

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -7,6 +7,6 @@ describe "current_age_for_birth_year method" do
     answer = current_year - birth_year
 
     age_of_person = current_age_for_birth_year(birth_year)
-    expect(age_of_person).to eq(anser)
+    expect(age_of_person).to eq(answer)
   end
 end
