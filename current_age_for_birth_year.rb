ea925cff713d1d36a15cf2c452ed10e9b717b537
michael white <michaeljwhite@gmail.com>
2019-01-27 07:37:52 +0000

    Done.

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -2,5 +2,5 @@ def
 current_age_for_birth_year(birth_year)
 # In Ruby, the last statement of a method is the 
 # return value, no return keyword necessary.
-2003 - birth_year
+Time.now.year - birth_year
 end
