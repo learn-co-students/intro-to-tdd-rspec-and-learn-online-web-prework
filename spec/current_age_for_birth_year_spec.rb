4c400299c3d1849cf58462575ba5e49825ac930e
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-04 15:10:50 +0000

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -8,6 +8,6 @@ describe "current_age_for_birth_year method" do
   end
 end
 def 
-  "current_age_for_birth_year"(birth_year)
- 2003 - birth_year
+  current_age_for_birth_year(birth_year)
+ 2003 - birth_year 
 end
